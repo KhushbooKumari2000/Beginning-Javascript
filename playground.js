0e24de5b0a4ee49585572398e1e1b71a408f285a
KhushbooKumari2000 <137270203+KhushbooKumari2000@users.noreply.github.com>
2023-06-21 20:57:27 +0530

    Update playground.js

--- playground.js
+++ playground.js
@@ -1,15 +1,13 @@
-/* 
+ 
 // Metacrafters - Beginning Javascript
-//
-// This is a javascript playground for testing your javascript code!
-// When you are ready to test, simply right click and select "Run Code"
-// to see the result print below. If you have more then one snippet of code,
-// you can highlight the code you want to test, and then right click and select "Run Code"
-*/
-
-// Enter your code below this line
-
-// Code example
-for(var i=1; i<=5; i++) {
- console.log(i);
+function mintNFT (_name, _eyeColor, _clothes, _bling) {
+   const NFT = {
+      "name": _name,
+      "eyeColor": _eyeColor,
+      "clothes": _clothes,
+      "bling": _bling
+   }
+   NFTs.push(NFT);
+   console.log("OUE:" + _name)
 }
+
